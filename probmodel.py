7ddc3915c757cefdcfd46f8cea25e5235aebf96b
bferrazAC <brunoferraz24@gmail.com>
2018-05-07 23:11:57 -0300

    Fixed rank values

--- probmodel.py
+++ probmodel.py
@@ -23,7 +23,7 @@ def normalize(tokens, stopwords):
 ### Term-frequency e dicionário
 
 #Calcula a frequência de cada token nos documentos e a frequencia dos documentos invertidas, retornando um dicionario com essas informações
-# retorna td, idf dos termos 
+# retorna td, idf dos termos
 def create_index( tokens, docs):
 	#gera frequencia dos termos
 	tf = {token : np.char.count([doc.lower() for doc in docs], token) for token in tokens}
@@ -64,6 +64,8 @@ def gen_tf_idf(tf,idf,n_docs, query = False):
 
 	return weight_matrix
 
+
+#######################
 # Calcula os vetores para os tokens
 # Retorna os vetores dos termos por documento e as normas dos documentos
 def calc_vect(tokens, n_docs):
@@ -75,7 +77,7 @@ def calc_vect(tokens, n_docs):
 		# vetor singular do documento
 		tokens_vect = []
 		for token in tokens:
-			# 
+			#
 			w = tokens[token][n]
 			tokens_vect.append(w)
 			# calculo da norma ( somatorio dos modulos )
@@ -88,18 +90,18 @@ def calc_vect(tokens, n_docs):
 
 	return docs_vect,docs_norms
 
-# Calcula ranking vetorial 
+# Calcula ranking vetorial
 # Retorna o ranking ordenado dos documentos
 def rank(tokens, q_tokens, n_docs):
 	# O ranking é uma lista de tuplas (peso, numero do documento)
 	rank_docs = [(0,n) for n in range(n_docs)]
 	# Calculo de vetores e normas para os docs e query
 	docs_vect, docs_norms = calc_vect(tokens,n_docs)
+	# print q_vect
 	q_vect, q_norms = calc_vect(q_tokens, 1)
 
 	for doc in range(n_docs):
 		# print docs_vect[doc]
-		# print q_vect
 		# print "llalalal"
 		r = np.dot(docs_vect[doc],q_vect)
 		r /= math.sqrt(docs_norms[doc]*q_norms)
@@ -109,47 +111,51 @@ def rank(tokens, q_tokens, n_docs):
 
 	return rank_docs
 
-### Probabilistico
-# 
-def estimate_len(doc, tf, index):
+####################################
+# Ranking Probabilistico
+#
+def estimate_len(tf, index):
 	length = 0
 	for token in tf:
 		length += tf[token][index]
 
 	return length
 
-#
+# 
 def bm25(freq, estm_len, avg):
-	return (K1 + 1)*freq / K1*( (1-B) + B*(estm_len/avg) ) +freq
+	return ((K1 + 1)*freq) / (K1*( (1-B) + B*(estm_len/avg) ) +freq)
 
-#
-def rank_sim(docs, q_tokens, tf, idf):
-	estm_len = [estimate_len(doc, tf, docs.index(doc)) for doc in docs]
-	print estm_len
+# 
+def rank_sim(num_docs, q_tokens, tf, idf):
+	estm_len = [estimate_len( tf, doc) for doc in range(num_docs)]
+	# print estm_len
 	avg_len = np.average(estm_len)
 
 	sim_list = []
-	
-	for doc in docs:
-		tokens_list = [t for t in q_tokens if t in doc]
+
+	for doc in range(num_docs):
+		tokens_list = [t for t in q_tokens if tf[t][doc] >= 1]
+		# print str(doc) + ' ' + str(tokens_list)
 		doc_sim = 0
-		j = docs.index(doc)
-		for token in q_tokens:
+		j = doc
+		for token in tokens_list:
 			b = bm25(tf[token][j], estm_len[j], avg_len)
-			sub_sentence = len(docs) - idf[token] + .5 / idf[token] + 0.5
+			sub_sentence = (num_docs - idf[token] + 0.5) / (idf[token] + 0.5)
 			doc_sim += b * math.log(sub_sentence, 2)
 		sim_list.append([doc_sim, j])
 
 	sim_list.sort()
+	sim_list.reverse()
 
 	return sim_list
 
 if __name__ == '__main__':
 
-	text = ['O peã e o caval são pec de xadrez. O caval é o melhor do jog.', 'A jog envolv a torr, o peã e o rei.', 'O peã lac o boi', 'Caval de rodei!', 'Polic o jog no xadrez.']
+	text = ['O peã e o caval são pec de xadrez. O caval é o melhor do jog.', 'A jog envolv a torr, o peã e o rei.',
+	 		'O peã lac o boi', 'Caval de rodei!', 'Polic o jog no xadrez.']
 	stopwords = ['a', 'o', 'e', 'é', 'de', 'do', 'no', 'são']
 	q = ['xadrez peã caval torr']
-	
+
 	# Trata os termos dos docs
 	tokens = normalize(tokenize(text), stopwords)
 	tf, idf = create_index(tokens, text)
@@ -163,10 +169,6 @@ if __name__ == '__main__':
 
 	# q_weighted_tokens = gen_tf_idf(q_tf, q_idf, len(text), query=True)
 
-	### Debug
-	# print weighted_tokens
-	# print q_weighted_tokens
-
 	### Vetorial
 	# rank_docs = rank(weighted_tokens,q_weighted_tokens,len(text))
 
@@ -177,7 +179,7 @@ if __name__ == '__main__':
 	# 		print str(text.index(doc)) + ": " + str(weighted_tokens[token][text.index(doc)])
 	# print "Score" "\t| " + "Doc No."
 	# for rank in rank_docs:
-	# 	print str(rank[0]) + "\t| " + str(rank[1]) 
+	# 	print str(rank[0]) + "\t| " + str(rank[1])
 
 	#### _Vetorial
 
@@ -185,8 +187,15 @@ if __name__ == '__main__':
 	global K1, B
 	K1 = 1
 	B = .75
-	rank_prob = rank_sim(text, q_tokens, tf, idf)
+
+	# print q_tokens
+	# tk_docs = [tokenize(doc) for doc in text]
+	# print 'test ' + str(tk_docs)
+	rank_prob = rank_sim(len(text), query, tf, idf)
 
 	print "Rank \t| Doc No."
 	for rank in rank_prob:
-		print str(rank[0]) + '\t| ' + str(rank[1])
+		if rank[0] > 0:	
+			print str(rank[0]) + '\t| ' + str(rank[1] + 1)
+
+	### _Probabilistico
